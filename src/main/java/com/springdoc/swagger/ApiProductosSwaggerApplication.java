7c81baa1a7e04eefa730c2bdc7ebb7bb8e8da948
mateo <mateo204r@gmail.com>
2024-02-01 17:07:45 -0500

    Api documentada

--- src/main/java/com/springdoc/swagger/ApiProductosSwaggerApplication.java
+++ src/main/java/com/springdoc/swagger/ApiProductosSwaggerApplication.java
@@ -1,7 +1,20 @@
 package com.springdoc.swagger;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.springdoc.swagger.models.Producto;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.SpecVersion;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.Schema;
 
 @SpringBootApplication
 public class ApiProductosSwaggerApplication {
@@ -10,4 +23,46 @@ public class ApiProductosSwaggerApplication {
 		SpringApplication.run(ApiProductosSwaggerApplication.class, args);
 	}
 
+	
+	  @Bean
+	  OpenAPI springShopOpenAPI() {
+		  
+		
+		 
+	      return new OpenAPI(SpecVersion.V31)
+	    		  .info(new Info()
+	    				  .title("Api Productos - Spring Doc")
+	    				  .version("0.0.1-SNAPSHOT")
+	    				  .description("Esta es una api que permite realizar operaciones CRUD en una base "
+	    				  		+ "de datos de productos en H2 database basado enla especificacion OpenAPI 3.1.0. Con el fin de aprender a documentar "
+	    				  		+ "mis APIs desarrolladas con spring boot.")
+	    				  .contact(new Contact()
+	    						  .name("Mateo Rodriguez")
+	    						  .email("mateo204r@gmail.com"))
+	    				  )
+	    		  ;
+	    		
+	              
+	  }
+	  
+	  /**
+	   * Agregando un nuevo schema a para el request body post de productos
+	   * @return
+	   */
+	  @Bean
+	   OpenApiCustomizer consumerTypeHeaderOpenAPICustomizer() {
+		  Schema<Producto> schema= new Schema<>();
+		  Map<String, Schema> properties= new HashMap<>();
+		
+		  properties.put("nombre", new Schema().type("string"));
+		  properties.put("precio", new Schema().type("number").format("double"));
+		  properties.put("fechaDeCreacion", new Schema().type("string").format("date-time"));
+		  schema.name("productoSave");
+		  schema.type("object");
+		  schema.properties(properties);
+		  
+		  return openApi -> openApi
+				  .components(openApi.getComponents())
+				  .schema("ProductoSave", schema);
+	  }
 }
--- src/main/java/com/springdoc/swagger/controllers/ProductoController.java
+++ src/main/java/com/springdoc/swagger/controllers/ProductoController.java
@@ -15,28 +15,47 @@ import org.springframework.web.bind.annotation.RestController;
 import com.springdoc.swagger.models.Producto;
 import com.springdoc.swagger.services.ProductoServices;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
+@Tag(name="Productos",description = "Todo sobre los prodcutos")
 public class ProductoController {
 
-	
+	@Operation(description = "Lista todos los productos disponibles ")
 	@GetMapping(value = "/list")
 	public List<Producto> listar(){
 		return productoServices.findAll();
 	}
 	
+
+	@Operation(description = "Obtiene un productos por id",
+			parameters = {@Parameter(name = "id",description = "Es el id del producto que será buscado")})
 	@GetMapping(value = "/view/{id}")
 	public Producto detalle(@PathVariable(name = "id") Long id){
 		return productoServices.findById(id);
 	}
 	
-	
+
+	@Operation(description = "Crea y guarda un nuevo producto",
+			requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+			content = @Content(mediaType = "application/json",schema = 
+			@Schema(ref = "#/components/schemas/ProductoSave"))))
 	@PostMapping(value="/save")
-	public ResponseEntity<Producto> saveProduct(@RequestBody Producto producto){
+	public ResponseEntity<Producto> saveProduct(@RequestBody  Producto producto){
 		
 		return new ResponseEntity<Producto>(productoServices.save(producto)
 				,HttpStatus.CREATED);
 	}
 	
+
+	@Operation(description = "Actualiza el producto",
+			parameters = {@Parameter(name = "id",description = "Es el id del producto que será actualizado")}
+			)
 	@PutMapping(value="/update")
 	public ResponseEntity<Producto> updateProduct(@RequestBody Producto producto){
 		return ResponseEntity
@@ -44,7 +63,9 @@ public class ProductoController {
 		
 	}
 	
-	
+
+	@Operation(description = "Elimina un producto por id",
+			parameters = {@Parameter(name = "id",description = "Es el id del producto que será eliminado")})
 	@DeleteMapping(value = "/delete/{id}")
 	public ResponseEntity<?> delteProduct(@PathVariable(name = "id")Long id) {
 		productoServices.deleteById(id);
